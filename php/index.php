9e28eb5b8ba4f22df78dcede163a3509730fa1c0
Shane Logsdon <shane@shanelogsdon.com>
2014-10-11 14:02:18 -0400

    adding php

--- php/index.php
+++ php/index.php
@@ -1,1 +1,1 @@
-<?phpprint'Hello World!';
+<?php$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die('Failed to create socket!');socket_bind($socket, 0, 3000);socket_listen($socket); $msg = '<html><head><title>Hello, world!</title></head><body>Hello, world!</body></html>'; for (;;) {    if ($client = socket_accept($socket)) {        socket_write($client, "HTTP/1.1 200 OK\r\n" .               "Content-length: " . strlen($msg) . "\r\n" .               "Content-Type: text/html; charset=UTF-8\r\n\r\n" .               $msg);    }}
