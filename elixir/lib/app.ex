9ce126a0dfd497af2f582283505710e21e99b498
Shane Logsdon <shane@shanelogsdon.com>
2014-10-11 20:44:47 -0400

    elixir: remove unnecessary cruft

--- elixir/lib/app.ex
+++ elixir/lib/app.ex
@@ -5,9 +5,17 @@ defmodule App do
     import Supervisor.Spec, warn: false
 
     children = [
+      worker(__MODULE__, [], function: :run)
     ]
 
     opts = [strategy: :one_for_one, name: App.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  def run do
+    Plug.Adapters.Cowboy.http App.Router, [], [port: 3000]
+    unless (Code.ensure_loaded?(IEx) && IEx.started?) do
+      :timer.sleep(:infinity)
+    end
   end
 end
